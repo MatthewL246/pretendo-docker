b3030fe5987e340e87ab54555d79a096464d73c9
Matthew Lopez <73856503+MatthewL246@users.noreply.github.com>
2023-12-19 13:58:45 -0500

    Set up Minio S3 virtual hosts

--- config/minio-init.sh
+++ config/minio-init.sh
@@ -5,9 +5,9 @@ set -eu
 mc alias set minio http://minio:9000 "$MINIO_ROOT_USER" "$MINIO_ROOT_PASSWORD"
 mc admin info minio
 
-buckets="mii paintings screenshots"
+buckets="pn-cdn"
 
-# Create a bucket named mii and allow public access
+# Create buckets allow public access
 for bucket in $buckets; do
     mc mb "minio/$bucket"
     mc anonymous set download "minio/$bucket"
