47a91661c1e1927e908d33ce4205fb69e6228785
Matthew Lopez <73856503+MatthewL246@users.noreply.github.com>
2023-12-20 17:33:29 -0500

    Small script updates

--- scripts/setup-minio-container.sh
+++ scripts/setup-minio-container.sh
@@ -7,9 +7,6 @@ minio_init_script=$(cat "$git_base/config/minio-init.sh")
 
 docker compose up -d minio
 
-# TODO: Find a better way to do this in case Minio takes longer to start up
-echo "Waiting for minio to be ready..."
-sleep 2
 docker compose exec minio sh -c "$minio_init_script"
 
 docker compose down minio
--- scripts/setup-mongodb-container.sh
+++ scripts/setup-mongodb-container.sh
@@ -7,6 +7,11 @@ mongodb_init_script=$(cat "$git_base/config/mongodb-init.js")
 
 docker compose up -d mongodb
 
+# This won't work in /docker-entrypoint-initdb.d/ because MongoDB is in a
+# special init state where it will refuse to resolve anything but localhost.
+# This needs to be run after it initializes.
+# https://github.com/docker-library/mongo/issues/339
+
 while ! docker compose exec mongodb mongosh --eval "db.adminCommand('ping')"; do
     echo "Waiting for mongodb to be ready..."
     sleep 1
