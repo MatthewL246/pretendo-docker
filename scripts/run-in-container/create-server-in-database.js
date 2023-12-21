57a9f834340cda711054df443cd0a84a50ccf841
Matthew Lopez <73856503+MatthewL246@users.noreply.github.com>
2023-12-21 18:11:54 -0500

    Rewrite account server database script outside of the patches

---
+++ scripts/run-in-container/create-server-in-database.js
@@ -0,0 +1,144 @@
+const mongoose = require("mongoose");
+const { connect } = require("./dist/database");
+const { Server } = require("./dist/models/server");
+
+async function runAsync() {
+    await connect();
+    await resetServers();
+
+    await createNexServer(
+        "Friend List",
+        "0003200",
+        ["0005001010001C00"],
+        process.env.COMPUTER_IP,
+        process.env.FRIENDS_PORT,
+        "dev",
+        1,
+        process.env.FRIENDS_AES_KEY
+    );
+    await createServiceServer(
+        "Miiverse",
+        "87cd32617f1985439ea608c2746e4610",
+        ["000500301001610A"],
+        "dev",
+        1,
+        process.env.MIIVERSE_AES_KEY
+    );
+    await createNexServer(
+        "Wii U Chat",
+        "1005A000",
+        ["000500101005A100"],
+        process.env.COMPUTER_IP,
+        process.env.WIIU_CHAT_PORT,
+        "dev",
+        1,
+        // Wii U Chat server doesn't seem to care about the AES key
+        "0".repeat(64)
+    );
+
+    await mongoose.connection.close();
+}
+
+runAsync().then(() => {
+    console.log("Done creating servers.");
+    process.exit(0);
+});
+
+async function resetServers() {
+    console.log("Deleting all servers...");
+    await Server.deleteMany({});
+    console.log("Servers collection reset.");
+}
+
+async function createNexServer(
+    service_name,
+    game_server_id,
+    title_ids,
+    ip,
+    port,
+    access_mode,
+    device,
+    aes_key
+) {
+    for (const arg of arguments) {
+        if (!arg && arg !== 0 && arg !== false) {
+            throw new Error(
+                `Missing argument in new NEX server:\n${JSON.stringify(
+                    arguments,
+                    null,
+                    2
+                )}`
+            );
+        }
+    }
+
+    const newServer = new Server({
+        service_name: service_name,
+        service_type: "nex",
+        game_server_id: game_server_id,
+        title_ids: title_ids,
+        ip: ip,
+        port: port,
+        access_mode: access_mode,
+        maintenance_mode: false,
+        device: device,
+        aes_key: aes_key,
+    });
+
+    console.log("Saving new nex server:");
+    console.log(newServer);
+
+    await newServer.save();
+}
+
+async function createServiceServer(
+    service_name,
+    client_id,
+    title_ids,
+    access_mode,
+    device,
+    aes_key
+) {
+    for (const arg of arguments) {
+        if (!arg && arg !== 0 && arg !== false) {
+            throw new Error(
+                `Missing argument in new service server:\n${JSON.stringify(
+                    arguments,
+                    null,
+                    2
+                )}`
+            );
+        }
+    }
+
+    const newServer = new Server({
+        service_name: service_name,
+        service_type: "service",
+        client_id: client_id,
+        title_ids: title_ids,
+        access_mode: access_mode,
+        maintenance_mode: false,
+        device: device,
+        aes_key: aes_key,
+    });
+
+    console.log("Saving new service server:");
+    console.log(newServer);
+
+    await newServer.save();
+}
+
+/* Notes:
+    Finding the right server on Wii U:
+        Client ID is for services
+        Game server ID is for nex servers
+    IP and port are only for nex servers
+    Service name and service type seem to be unused, so just for documentation?
+    Title ID is used for nex and service 3DS servers, not client or game server IDs
+    Access mode separates servers into prod and test, based on to PNID access level
+    Maintenance mode is self-explanatory
+    Device seems to be 1 for Wii U, 2 for 3DS, and 3 for internal API usage?
+        Except this is only used for the Wii U nex and service tokens, the 3DS
+        tokens are harcoded to 2 and internal API are hardcoded to 3
+    AES key is a 32-byte hex string used for creating tokens
+*/
--- scripts/update-account-servers-database.sh
+++ scripts/update-account-servers-database.sh
@@ -8,8 +8,9 @@ for _ in $(seq 1 64); do
 done
 
 git_base=$(git rev-parse --show-toplevel)
-necessary_environment="friends miiverse-api wiiu-chat"
+create_server_script=$(cat "$git_base/scripts/run-in-container/create-server-in-database.js")
 
+necessary_environment="friends miiverse-api wiiu-chat"
 . "$git_base/environment/system.local.env"
 for environment in $necessary_environment; do
     . "$git_base/environment/$environment.env"
@@ -18,18 +19,12 @@ done
 
 docker compose up -d account
 
-docker compose exec account node /app/dist/create-server-in-database.js reset
-
-docker compose exec account node /app/dist/create-server-in-database.js nex \
-    "Friend List" "00003200" "0005001010001C00" "$COMPUTER_IP" \
-    "$PN_FRIENDS_AUTHENTICATION_SERVER_PORT" "dev" "$PN_FRIENDS_CONFIG_AES_KEY"
-docker compose exec account node /app/dist/create-server-in-database.js service \
-    "Miiverse" "87cd32617f1985439ea608c2746e4610" "000500301001610A" "dev" \
-    "$PN_MIIVERSE_API_CONFIG_AES_KEY"
-# Wii U Chat server doesn't seem to care about the AES key
-docker compose exec account node /app/dist/create-server-in-database.js nex \
-    "Wii U Chat" "1005A000" "000500101005A100" "$COMPUTER_IP" \
-    "$PN_WIIU_CHAT_AUTHENTICATION_SERVER_PORT" "dev" "$null_aes_key"
+docker compose exec -e COMPUTER_IP="$COMPUTER_IP" \
+    -e FRIENDS_PORT="$PN_FRIENDS_AUTHENTICATION_SERVER_PORT" \
+    -e FRIENDS_AES_KEY="$PN_FRIENDS_CONFIG_AES_KEY" \
+    -e MIIVERSE_AES_KEY="$PN_MIIVERSE_API_CONFIG_AES_KEY" \
+    -e WIIU_CHAT_PORT="$PN_WIIU_CHAT_AUTHENTICATION_SERVER_PORT" \
+    account node -e "$create_server_script"
 
 # TODO: Script should a PNID's access_level to 3 and server_access_level to dev
 # so users can access the servers
