7275d3ba2663527344d6d793a345806088dfaf06
Matthew Lopez <73856503+MatthewL246@users.noreply.github.com>
2024-05-11 17:08:48 -0400

    Create a simple server update script

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# shellcheck source=./scripts/internal/framework.sh
+source "$(dirname "$(realpath "$0")")/scripts/internal/framework.sh"
+set_description "This updates the Pretendo environment to the latest version."
+parse_arguments "$@"
+
+# Without resetting, Git may have merge conflicts in the submodules because of the applied patches
+git submodule foreach "git reset --hard" >/dev/null
+git pull
+
+exec "$git_base_dir/setup.sh"
