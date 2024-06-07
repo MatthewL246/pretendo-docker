436fedc28959b38b929e9a5a394d9e44c0525450
Matthew Lopez <73856503+MatthewL246@users.noreply.github.com>
2024-06-07 12:50:19 -0400

    Add hackless Nintendo CA G3 certificate download method

--- scripts/patch-sssl-certificate.sh
+++ scripts/patch-sssl-certificate.sh
@@ -5,9 +5,19 @@ source "$(dirname "$(realpath "$0")")/internal/framework.sh"
 set_description "This creates a patched Nintendo CA G3 SSL certificate using the SSSL patcher."
 parse_arguments "$@"
 
+expected_certificate_hash="220a4fba273a033c0edd7ae0993b3737215fc05ff972fcb5472aab6dbece6409"
+
 if [[ ! -f "$git_base_dir/console-files/CACERT_NINTENDO_CA_G3.der" ]]; then
     print_error "Certificate CACERT_NINTENDO_CA_G3.der not found in the console-files directory. Please dump it from \
 \"/storage_mlc/sys/title/0005001b/10054000/content/scerts/CACERT_NINTENDO_CA_G3.der\"."
+    exit 1
+fi
+
+certificate_hash=$(sha256sum "$git_base_dir/console-files/CACERT_NINTENDO_CA_G3.der" | cut -d ' ' -f 1)
+if [[ "$certificate_hash" = "$expected_certificate_hash" ]]; then
+    print_success "Found valid Nintendo CA G3 certificate."
+else
+    print_error "Nintendo CA G3 certificate has the wrong hash! Try dumping it again."
     exit 1
 fi
 
