7f4968ba3317542061714a1f1b2e197c107e5e23
Matthew Lopez <73856503+MatthewL246@users.noreply.github.com>
2024-06-27 10:42:23 -0400

    Fix migrations script on first run

--- scripts/internal/migrations.sh
+++ scripts/internal/migrations.sh
@@ -8,11 +8,12 @@ print_info "Running migrations..."
 
 load_dotenv postgres.env postgres.local.env
 
-# Migrate friends to the nex-go rewrite
-migration=$(cat "$git_base_dir/scripts/run-in-container/friends-nex-go-rewrite-migration.sql")
-
-compose_no_progress up -d friends
-# shellcheck disable=SC2046
-docker compose exec postgres psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d friends -c "$migration" $(if_not_verbose --quiet)
+compose_no_progress up -d postgres
+if [[ $(docker compose exec postgres psql -U "$POSTGRES_USER" -d friends -c "SELECT schema_name FROM information_schema.schemata WHERE schema_name = '3ds';") == *"(1 row)" ]]; then
+    print_info "Migrating friends to the nex-go rewrite..."
+    migration=$(cat "$git_base_dir/scripts/run-in-container/friends-nex-go-rewrite-migration.sql")
+    # shellcheck disable=SC2046
+    docker compose exec postgres psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d friends -c "$migration" $(if_not_verbose --quiet)
+fi
 
 print_success "Migrations are complete."
